0547cf67b7b7d9ae74bf2058a42904db915c5560
Abhinav Menon <abhinavmenon54@gmail.com>
2025-05-21 10:29:03 -0400

    Fixed Indexing Issues

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,14 +8,24 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCQOOSCs7a76eBN8BFoLbIZpiDYa8bxzcw",
+  authDomain: "echohire-670ae.firebaseapp.com",
+  projectId: "echohire-670ae",
+  storageBucket: "echohire-670ae.firebasestorage.app",
+  messagingSenderId: "152589303555",
+  appId: "1:152589303555:web:766893b9fd09e633ea3336",
+  measurementId: "G-TF2DQL5E03",
 };
 
 // Initialize Firebase
@@ -24,3 +34,25 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCQOOSCs7a76eBN8BFoLbIZpiDYa8bxzcw",
+//   authDomain: "echohire-670ae.firebaseapp.com",
+//   projectId: "echohire-670ae",
+//   storageBucket: "echohire-670ae.firebasestorage.app",
+//   messagingSenderId: "152589303555",
+//   appId: "1:152589303555:web:766893b9fd09e633ea3336",
+//   measurementId: "G-TF2DQL5E03",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
